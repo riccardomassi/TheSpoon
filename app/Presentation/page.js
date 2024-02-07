c679da628af0bb985f9c90f3c9ee3bad8499ac76
riccardomassi <rickymassi@gmail.com>
2024-02-07 14:23:39 +0100

    inizio progetto, inserita navbar e pagine Presentation e Search Engine

---
+++ app/Presentation/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PresentationPage = () => {
+	return (
+		<div className="text-4xl flex w-screen h-screen bg-slate-100 dark:bg-slate-800 flex-auto items-center justify-center">
+			Presentation
+		</div>
+	);
+};
+
+export default PresentationPage;
---
+++ app/page.js
@@ -0,0 +1,9 @@
+import PresentationPage from './Presentation/page';
+
+export default function Home() {
+	return (
+		<div className="text-4xl flex w-screen h-screen bg-slate-100 dark:bg-slate-800 flex-auto items-center justify-center">
+			<PresentationPage />
+		</div>
+	);
+}
