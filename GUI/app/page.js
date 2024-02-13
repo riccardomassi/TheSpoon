80448dfb2c1db9bc12d16252b13272355f40eb4e
riccardomassi <rickymassi@gmail.com>
2024-02-13 17:19:22 +0100

    modificati e aggiunti file per la comunicazione tra GUI e server

--- GUI/app/page.js
+++ GUI/app/page.js
@@ -1,13 +1,11 @@
-import Searchbar from '@/components/SearchBar/Searchbar';
-import Results from '@/components/Results/Results';
+import SearchEngine from '@/components/SearchEngine/SearchEngine';
 import Topbar from '@/components/Topbar/Topbar';
 
 export default function Home() {
 	return (
 		<div className="flex flex-col w-screen h-screen bg-slate-100 dark:bg-slate-800 items-center justify-center">
+			<SearchEngine />
 			<Topbar />
-			<Searchbar />
-			<Results />
 		</div>
 	);
 }
