fb8ad132e6ca142a90af8a00cf2a6a21ac83ec00
riccardomassi <rickymassi@gmail.com>
2024-02-14 15:25:54 +0100

    test di prova

--- GUI/components/SearchBar/Searchbar.js
+++ GUI/components/SearchBar/Searchbar.js
@@ -7,15 +7,16 @@ import { Rating } from '@mui/material';
 const Searchbar = ({ onSearchResults }) => {
 	const [rating, setRating] = useState(1);
 	const [checked, setChecked] = useState(false);
-	const [sentiment, setSentiment] = useState('Positive');
+	const [sentiment, setSentiment] = useState('');
 	const [searchValue, setSearchValue] = useState('');
+	const [sorting, setSorting] = useState('Defualt');
 
 	const handleAutoexp = (event) => {
 		setChecked(event);
 	};
 
-	const handleSentiment = (event) => {
-		setSentiment(event.target.value);
+	const handleSorting = (event) => {
+		setSorting(event.target.value);
 	};
 
 	const handleSearchValue = (event) => {
@@ -33,7 +34,7 @@ const Searchbar = ({ onSearchResults }) => {
 			};
 
 			// Effettua la chiamata API per eseguire la query e avere la risposta
-			const response = await fetch('http://localhost:5005/execute_query', {
+			const response = await fetch('http://localhost:5000/api/search', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
@@ -50,44 +51,69 @@ const Searchbar = ({ onSearchResults }) => {
 	};
 
 	return (
-		<div className="w-1/2 absolute top-20 flex flex-col">
-			<div>
-				<label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
-					Search
-				</label>
-				<div className="relative">
-					<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-						<Search />
-					</div>
-					<input
-						className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
-						type="text"
-						id="default-search"
-						placeholder="Search..."
-						required
-						value={searchValue}
-						onChange={handleSearchValue}
-					/>
-					<button
-						className="text-white absolute end-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-700 focus:outline-non font-medium rounded-lg text-sm px-4 py-2"
-						onClick={apiRequest}
-					>
+		<div className="w-full absolute top-20 flex flex-col">
+			<div className="flex flex-row justify-between">
+				<div className="w-full mx-4">
+					<label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
 						Search
-					</button>
+					</label>
+					<div className="relative">
+						<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+							<Search />
+						</div>
+						<input
+							className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+							type="text"
+							id="default-search"
+							placeholder="Search..."
+							required
+							value={searchValue}
+							onChange={handleSearchValue}
+						/>
+						<button
+							className="text-white absolute end-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-700 focus:outline-non font-medium rounded-lg text-sm px-4 py-2"
+							onClick={apiRequest}
+						>
+							Search
+						</button>
+					</div>
+				</div>
+				<div className="w-full mx-4">
+					<label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+						Search
+					</label>
+					<div className="relative">
+						<div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+							<Search />
+						</div>
+						<input
+							className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+							type="text"
+							id="default-search"
+							placeholder="Search..."
+							required
+							value={searchValue}
+							onChange={handleSearchValue}
+						/>
+						<button
+							className="text-white absolute end-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-700 focus:outline-non font-medium rounded-lg text-sm px-4 py-2"
+							onClick={apiRequest}
+						>
+							Search
+						</button>
+					</div>
 				</div>
 			</div>
-			<div className="flex flex-row justify-between mt-6">
+			<div className="flex flex-row w-1/2 justify-between mt-6">
 				<div>
 					<select
-						value={sentiment}
-						onChange={handleSentiment}
+						value={sorting}
+						onChange={handleSorting}
 						className="rounded p-2 bg-slate-300 dark:bg-slate-600 hover:bg-slate-200 hover:dark:bg-slate-500 text-black dark:text-white"
 					>
-						<option value={'VeryPositive'}>Very Positive</option>
-						<option value={'Positive'}>Positive</option>
-						<option value={'Neutral'}>Neutral</option>
-						<option value={'Negative'}>Negative</option>
-						<option value={'VeryNegative'}>Very Negative</option>
+						<option value={'Default'}>Default</option>
+						<option value={'ReviewDate'}>Review Date</option>
+						<option value={'ReviewStars'}>Review Stars</option>
 					</select>
 				</div>
 				<div className="flex flex-col items-center">
--- __main__.py
+++ __main__.py
@@ -1,14 +1,30 @@
-from flask import *
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/api/search')
+@app.route('/api/search', methods=['GET', 'POST'])
 def callOnQuerySearch():
-    request_data = request.get_json()
+    if request.method == 'POST':
+        try:
+            # Parse JSON data from the request body
+            request_data = request.get_json()
 
-    print(request_data)
+            # Print the received data
+            print("Received data:", request_data)
+
+            # Your logic here using the received data
+
+            return jsonify({"status": "success", "message": "Data received successfully"})
+        except Exception as e:
+            print("Error parsing JSON data:", str(e))
+            return jsonify({"status": "error", "message": "Error parsing JSON data"})
+    else:
+        return jsonify({"status": "error", "message": "Only POST requests are supported"})
+    
     #documents = querySearch(text, minStarRating,sortTags, correctedQuery, useQueryExpansion, sentimentTags, useDefaultRanking, useOrGroup, resultLimit)
     #return documents
 
